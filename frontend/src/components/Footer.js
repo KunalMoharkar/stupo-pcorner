4ca43d7b9914de247652c631e5c038de81f7a708
theViz343 <vishwesh103@gmail.com>
2020-10-22 17:14:53 +0530

    Added footer and logo-sm

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../css/Footer.css'
 import '../css/homepage.css'
+import icon_sm from '../images/icon_sm.png'
 class Footer extends React.Component {
 
   constructor(props) {
@@ -16,44 +17,55 @@ class Footer extends React.Component {
 
     return(
       <footer class="footer">
-  <div class="container">
-      <div class="row inner">
-
-          <div class="col-xs-12 col-sm-4 col-md-3">
-              <a href="http://studentportal.vnit.ac.in">
-                  <img class="logo" style={{borderRadius: '4px'}} src="" alt="logo" width="140" />
-              </a>
-              <p>A platform that provides insight into the college life at VNIT.</p>
-          </div>
-          <div class="col-md-1"></div>
-          <div class="col-xs-12 col-sm-4 col-md-4">
-              <h3 class="g-color--white-opacity">Contact Us</h3>
-              <p class="g-color--white-opacity-light">Visvesvaraya National Institute of Technology, South Ambazari Rd, Ambazari, Nagpur, Maharashtra 440010</p>
-          </div>
-          <div class="col-md-1"></div>
-          <div class="col-sm-12 col-sm-4 col-md-3">
-            <h3 class="g-color--white-opacity">Follow Us</h3>
-              <div class="actions">
-                  <a class="action-link" rel="noreferrer" href="https://www.instagram.com/studentportalvnit/" target="_blank">
-                    <i class="g-padding-r-5--xs fa fa-instagram"></i>
-                    <span class="g-display-none--xs g-display-inline-block--sm">Instagram</span>
-                </a>
-              </div>
-              <div class="actions">
-                  <a class="action-link" rel="noreferrer" href="https://www.linkedin.com/company/student-portal-vnit" target="_blank">
-                    <i class="g-padding-r-5--xs fa fa-linkedin"></i>
-                    <span class="g-display-none--xs g-display-inline-block--sm">LinkedIn</span>
-                </a>
-              </div>
-              <div class="actions">
-                  <a class="action-link" rel="noreferrer" href="mailto:studentportal@students.vnit.ac.in" target="_blank">
-                    <i class="g-padding-r-5--xs fa fa-envelope"></i>
-                    <span class="g-display-none--xs g-display-inline-block--sm">Mail</span>
-                </a>
-              </div>
-          </div>
+  <div className="inner">
+  <div className="row company">
+    <img alt="logo" className="logo" src={icon_sm} style={{borderRadius: '4px'}} width={60} />
+    <p className="caption">Student Portal <br /> VNIT</p>
+  </div>
+  <div className="row"><div className="col-xs-12">
+      <a className="social action-link" href="https://www.instagram.com/studentportalvnit/" rel="noreferrer" target="_blank">
+        <i className="g-padding-r-5--xs fa fa-instagram" /> <span className="g-display-none--xs g-display-inline-block--sm">Instagram</span></a>
+      <a className="social action-link" href="https://www.linkedin.com/company/student-portal-vnit" rel="noreferrer" target="_blank">
+        <i className="g-padding-r-5--xs fa fa-linkedin" /> <span className="g-display-none--xs g-display-inline-block--sm">LinkedIn</span></a>
+      <a className="social action-link" href="mailto:studentportal@students.vnit.ac.in" rel="noreferrer" target="_blank">
+        <i className="g-padding-r-5--xs fa fa-envelope" /> <span className="g-display-none--xs g-display-inline-block--sm">Mail</span></a>
     </div>
   </div>
+  <div className="row footer-body">
+    <div className="col-xs-12 col-sm-4">
+      <h3 className="g-color--white-opacity">Locate Us</h3>
+      <p className="g-color--white-opacity-light">Visvesvaraya National Institute of Technology, South Ambazari Rd, Ambazari, Nagpur, Maharashtra 440010</p>
+    </div>
+    <div className="col-sm-12 col-sm-4">
+      <h3 className="g-color--white-opacity">Platforms</h3>
+      <div className="actions">
+        <a className="action-link" href="https://www.instagram.com/studentportalvnit/" rel="noreferrer" target="_blank">
+          <span className="g-display-none--xs g-display-inline-block--sm">Experiences</span></a>
+      </div>
+      <div className="actions">
+        <a className="action-link" href="https://www.linkedin.com/company/student-portal-vnit" rel="noreferrer" target="_blank">
+          <span className="g-display-none--xs g-display-inline-block--sm">Clubs</span></a>
+      </div>
+    </div>
+    <div className="col-sm-12 col-sm-4">
+      <h3 className="g-color--white-opacity">Quick Links</h3>
+      <div className="actions">
+        <a className="action-link" href="mailto:studentportal@students.vnit.ac.in" rel="noreferrer" target="_blank">
+          <span className="g-display-none--xs g-display-inline-block--sm">About Us</span></a>
+      </div>
+      <div className="actions"><a className="action-link" href="mailto:studentportal@students.vnit.ac.in" rel="noreferrer" target="_blank">
+          <span className="g-display-none--xs g-display-inline-block--sm">Testimonials</span></a>
+      </div>
+      <div className="actions">
+        <a className="action-link" href="mailto:studentportal@students.vnit.ac.in" rel="noreferrer" target="_blank">
+          <span className="g-display-none--xs g-display-inline-block--sm">Join Us</span></a>
+      </div>
+      <div className="actions" />
+      <div className="actions" />
+    </div>
+  </div>
+</div>
+
     <div class="copyright">
         <div class="" style={{position: 'relative', textAlign: 'center'}}>
             <p>© 2016-20 Copyright | Made with <i class="fa fa-heart"></i> by team Student Portal, VNIT</p>
